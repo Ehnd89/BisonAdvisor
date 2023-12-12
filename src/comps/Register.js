ecccc62a10b499b072ec16d8b38a14f5d4d81d90
Ehnd89 <amy.nyemah@bison.howard.edu>
2023-12-12 17:39:57 -0500

    WIP

---
+++ src/comps/Register.js
@@ -0,0 +1,50 @@
+import React from "react";
+import "./register.css";
+const imageUrl = 'https://upload.wikimedia.org/wikipedia/en/b/b4/Howard_Bison_logo.svg';
+
+export function Register(){
+    return(
+        <>
+        <div className="Centered">
+        <div className="HeaderBox">
+            <p className="BisonAdvisor">Bison Advisor</p>
+        </div>
+
+        <div className="Box">
+            <div className="LogoContainer">
+                <img
+                    src={imageUrl}
+                    alt="Howard Bison Logo"
+                    className="Logo"
+                />
+            </div>
+
+            <form>
+                <label htmlFor="email">Email:</label>
+                <input type="text" id="email" name="email" />
+
+                <label htmlFor="name">Name:</label>
+                <input type="text" id="name" name="name" />
+
+                <label htmlFor="password">Password:</label>
+                <input type="password" id="password" name="password" />
+
+                <label htmlFor="password">Re-enter Password:</label>
+                <input type="password" id="password" name="password" />
+
+                <div className="registerbtn">
+                    <button type="submit" href="/setup">Register</button>
+                </div>
+
+            </form>
+            <div className="signInLine">
+            Already Registered?
+            <a href="/login" className="Loginbtn">
+                Login
+            </a>
+            </div>
+        </div>
+    </div>
+    </>
+    );
+}
---
+++ src/comps/Setup.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./setup.css";
+
+function Setup() {
+    return (
+    <div className='QuestionBox'>
+        Setup
+    </div>
+    );
+}
+
+export default Setup
